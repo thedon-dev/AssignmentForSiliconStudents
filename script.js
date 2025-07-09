5b25812b8e1d2e0f4ae0de42af3fe30958cc1c30
Victor Jack <67293106+thedon-dev@users.noreply.github.com>
2025-07-09 11:47:19 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var navBar = document.getElementById("navLinks");
+
+function openNav() {
+  if (navBar.style.display === "block") {
+    navBar.style.display = "none";
+  } else {
+    navBar.style.display = "block";
+  }
+}

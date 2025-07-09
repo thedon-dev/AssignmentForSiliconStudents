dd443c6ce2701ccc9abd6d9cbd5aebfe2d7dfc35
Victor Johnson <okikevictorodinaka@gmail.com>
2025-07-09 06:59:29 -0700

    added navbar scroll

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 var navBar = document.getElementById("navLinks");
 
-function openNav() {
-  if (navBar.style.display === "block") {
-    navBar.style.display = "none";
-  } else {
-    navBar.style.display = "block";
-  }
-}
+// function openNav() {
+//   if (navBar.style.display === "block") {
+//     navBar.style.display = "none";
+//   } else {
+//     navBar.style.display = "block";
+//   }
+// }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const navBtn = document.querySelector(".navBtn");
+  const navLinks = document.getElementById("navLinks");
+
+  navBtn.addEventListener("click", function () {
+    navLinks.classList.toggle("active");
+  });
+});

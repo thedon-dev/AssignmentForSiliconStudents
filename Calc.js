9037ff6da9ad012a51ca622404ce8255bb863803
Victor Jack <67293106+thedon-dev@users.noreply.github.com>
2025-07-16 11:08:55 +0100

    Finished the calculator

--- Calc.js
+++ Calc.js
@@ -1,28 +1,26 @@
-const display = document.getElementById('calcDisplay');
+const display = document.getElementById("calcDisplay");
 
 let operator = "";
 
 function showNum(value) {
-    if (value === "") {
-        operator = "";
-        display.textContent = "0";
-        return;
-    }
+  if (value === "") {
+    operator = "";
+    display.textContent = "0";
+    return;
+  }
 
-    if (typeof value === "undefined") {
-        operator = operator.slice(0, -1);
-        display.textContent = operator || "0";
-        return;
-    }
+  if (typeof value === "undefined") {
+    operator = operator.slice(0, -1);
+    display.textContent = operator || "0";
+    return;
+  }
 
-    if (value === "=") {
-        operator = eval(operator).toString();
-        display.textContent = operator;
-        return;
-    }
-
-    operator += value;
+  if (value === "=") {
+    operator = eval(operator).toString();
     display.textContent = operator;
+    return;
+  }
 
+  operator += value;
+  display.textContent = operator;
 }
-

252f267867967e4260cc4b192d4ceaee30b3bf02
Codesheriff-p <princeamaeze04@gmail.com>
2025-07-16 13:27:28 +0100

    completed

--- Calc.js
+++ Calc.js
@@ -7,30 +7,30 @@ let operator = "";
 toggleBtn.addEventListener("click", toggleMode);
 
 function toggleMode() {
-  mainBg.classList.toggle("bg-white")
-  mainBg.classList.toggle("calDarkMode")
+    mainBg.classList.toggle("bg-white")
+    mainBg.classList.toggle("calDarkMode")
 
 }
 
 function showNum(value) {
-  if (value === "") {
-    operator = "";
-    display.textContent = "";
-    return;
-  }
+    if (value === "") {
+        operator = "";
+        display.textContent = "";
+        return;
+    }
 
-  if (typeof value === "undefined") {
-    operator = operator.slice(0, -1);
+    if (typeof value === "undefined") {
+        operator = operator.slice(0, -1);
+        display.textContent = operator;
+        return;
+    }
+
+    if (value === "=") {
+        operator = eval(operator).toString();
+        display.textContent = operator;
+        return;
+    }
+
+    operator += value;
     display.textContent = operator;
-    return;
-  }
-
-  if (value === "=") {
-    operator = eval(operator).toString();
-    display.textContent = operator;
-    return;
-  }
-
-  operator += value;
-  display.textContent = operator;
 }
